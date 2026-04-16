a47223a6fe2b873ebead5fe14a9bc4f157d76cae
Katie Hill <khill.fr.gr@gmail.com>
2026-04-16 16:42:28 +0100

    Add tab buttons for Growing & Planning

--- apps/phytovida/src/pages/Dashboard.tsx
+++ apps/phytovida/src/pages/Dashboard.tsx
@@ -1,20 +1,37 @@
 import { Button } from "@repo/ui/components/button";
 import { Link } from "react-router";
+import { useState } from "react";
 
+type Tab = "growing" | "planning";
 
 export default function Dashboard() {
+    const [activeTab, setActiveTab] = useState<Tab>("growing");
 
     return (
     <div className="h-full grid place-content-center gap-6">
       <h1 className="text-headline">Welcome, username</h1>
+
       <div className="flex justify-center gap-3">
-        <Button className="rounded-full" variant="secondary" asChild>
-          <Link to="/">Growing</Link>
+
+        <Button className="rounded-full" 
+        variant={activeTab === "growing" ? "default" : "secondary"} 
+        onClick={() => setActiveTab ("growing")}>
+          Growing
         </Button>
-        <Button className="rounded-full" variant="outline" asChild>
-          <Link to="/">Planning</Link>
+
+        <Button className="rounded-full bg-accent3"
+        variant={activeTab === "planning" ? "default" : "secondary"} 
+        onClick={() => setActiveTab ("planning")}>
+          Planning
         </Button>
+
       </div>
+
+        <div>
+        {activeTab === "growing" && <p>My Garden goes here</p>}
+        {activeTab === "planning" && <p>Planting Queue goes here</p>}
+      </div>
+
     </div>
     );
 }
