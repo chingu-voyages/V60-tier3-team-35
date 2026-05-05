856b34137c02852ff01fcbcc2d74e6fc167da8dc
Katie Hill <khill.fr.gr@gmail.com>
2026-05-05 14:57:41 +0100

    Add PlantLibraryCard component

---
+++ apps/phytovida/src/components/PlantLibraryCard.tsx
@@ -0,0 +1,9 @@
+import { useUser } from "@clerk/clerk-react";
+import { Droplets, Thermometer } from "lucide-react";
+import CreateUserPlantForm from "@/components/UserPlantCreation/CreateUserPlantForm";
+import type { DbPlant } from "@repo/types";
+
+export function PlantLibraryCard({ id, name, imageURL, hardiness, watering }: DbPlant) {
+    
+
+}
